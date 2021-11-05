3826ad14d8d63e9128fe82f82a8c6a7344a8379c
corretto-github-robot <no-reply@amazon.com>
2021-11-05 21:16:09 +0000

    Update to get the upstream build from remote tags

--- .github/actions/update-version/update-version.sh
+++ .github/actions/update-version/update-version.sh
@@ -2,7 +2,8 @@
 
 set -x
 
-VERSION_BRANCH=$1
+UPSTREAM_REMOTE=upstream-$1
+VERSION_BRANCH=$2
 
 git config user.email "no-reply@amazon.com"
 git config user.name "corretto-github-robot"
@@ -12,14 +13,17 @@ git checkout ${VERSION_BRANCH}
 # Load the current OpenJDK version
 source make/conf/version-numbers.conf
 
+BUILD_NUMBER=$(git ls-remote --tags ${UPSTREAM_REMOTE} |grep jdk-${DEFAULT_VERSION_FEATURE}+ | grep -vE "(-ga|{})$" | cut -d+ -f 2 |sort -n |tail -1)
+
 # Load the current Corretto version
 CURRENT_VERSION=$(cat version.txt)
 
-if [[ ${CURRENT_VERSION} == ${DEFAULT_VERSION_FEATURE}.${DEFAULT_VERSION_INTERIM}.${DEFAULT_VERSION_UPDATE}.* ]]; then
+if [[ ${CURRENT_VERSION} == ${DEFAULT_VERSION_FEATURE}.${DEFAULT_VERSION_INTERIM}.${DEFAULT_VERSION_UPDATE}.${BUILD_NUMBER}.* ]]; then
     echo "Corretto version is current."
 else
     echo "Updating Corretto version"
-    echo "${DEFAULT_VERSION_FEATURE}.${DEFAULT_VERSION_INTERIM}.${DEFAULT_VERSION_UPDATE}.0.0" > version.txt
-    git commit -m "Update Corretto version to match upstream: ${DEFAULT_VERSION_FEATURE}.${DEFAULT_VERSION_INTERIM}.${DEFAULT_VERSION_UPDATE}" version.txt
+    NEW_VERSION="${DEFAULT_VERSION_FEATURE}.${DEFAULT_VERSION_INTERIM}.${DEFAULT_VERSION_UPDATE}.${BUILD_NUMBER}.1"
+    echo  "${NEW_VERSION}" > version.txt
+    git commit -m "Update Corretto version to match upstream: ${NEW_VERSION}" version.txt
     git push origin ${VERSION_BRANCH}
 fi
