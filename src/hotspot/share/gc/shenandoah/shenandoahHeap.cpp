alvdavi/shenandoah

Split Generational Shenandoah memory pools

src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -524,7 +524,9 @@ ShenandoahHeap::ShenandoahHeap(ShenandoahCollectorPolicy* policy) :
   _verifier(NULL),
   _phase_timings(NULL),
   _monitoring_support(NULL),
-  _memory_pool(NULL),
+  _global_memory_pool(NULL),
+  _young_gen_memory_pool(NULL),
+  _old_gen_memory_pool(NULL),
   _stw_memory_manager("Shenandoah Pauses", "end of GC pause"),
   _cycle_memory_manager("Shenandoah Cycles", "end of GC cycle"),
   _gc_timer(new (ResourceObj::C_HEAP, mtGC) ConcurrentGCTimer()),
@@ -2573,9 +2575,18 @@ bool ShenandoahHeap::should_inject_alloc_failure() {
 }
 
 void ShenandoahHeap::initialize_serviceability() {
-  _memory_pool = new ShenandoahMemoryPool(this);
-  _cycle_memory_manager.add_pool(_memory_pool);
-  _stw_memory_manager.add_pool(_memory_pool);
+  if (mode()->is_generational()) {
+    _young_gen_memory_pool = new ShenandoahYoungGenMemoryPool(this);
+    _old_gen_memory_pool = new ShenandoahOldGenMemoryPool(this);
+    _cycle_memory_manager.add_pool(_young_gen_memory_pool);
+    _cycle_memory_manager.add_pool(_old_gen_memory_pool);
+    _stw_memory_manager.add_pool(_young_gen_memory_pool);
+    _stw_memory_manager.add_pool(_old_gen_memory_pool);
+  } else {
+    _global_memory_pool = new ShenandoahGlobalMemoryPool(this);
+    _cycle_memory_manager.add_pool(_global_memory_pool);
+    _stw_memory_manager.add_pool(_global_memory_pool);
+  }
 }
 
 GrowableArray<GCMemoryManager*> ShenandoahHeap::memory_managers() {
@@ -2587,12 +2598,17 @@ GrowableArray<GCMemoryManager*> ShenandoahHeap::memory_managers() {
 
 GrowableArray<MemoryPool*> ShenandoahHeap::memory_pools() {
   GrowableArray<MemoryPool*> memory_pools(1);
-  memory_pools.append(_memory_pool);
+  if (mode()->is_generational()) {
+    memory_pools.append(_young_gen_memory_pool);
+    memory_pools.append(_old_gen_memory_pool);
+  } else {
+    memory_pools.append(_global_memory_pool);
+  }
   return memory_pools;
 }
 
 MemoryUsage ShenandoahHeap::memory_usage() {
-  return _memory_pool->get_memory_usage();
+  return MemoryUsage(_initial_size, used(), committed(), max_capacity());
 }
 
 ShenandoahRegionIterator::ShenandoahRegionIterator() :

src/hotspot/share/gc/shenandoah/shenandoahMemoryPool.cpp
@@ -24,15 +24,24 @@
 
 #include "precompiled.hpp"
 #include "gc/shenandoah/shenandoahMemoryPool.hpp"
+#include "gc/shenandoah/shenandoahYoungGeneration.hpp"
+#include "gc/shenandoah/shenandoahOldGeneration.hpp"
 
-ShenandoahMemoryPool::ShenandoahMemoryPool(ShenandoahHeap* heap) :
-        CollectedMemoryPool("Shenandoah",
+
+ShenandoahMemoryPool::ShenandoahMemoryPool(ShenandoahHeap* heap,
+                                           const char* name) :
+        CollectedMemoryPool(name,
                             heap->initial_capacity(),
                             heap->max_capacity(),
                             true /* support_usage_threshold */),
                             _heap(heap) {}
 
-MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
+ShenandoahGlobalMemoryPool::ShenandoahGlobalMemoryPool(ShenandoahHeap* heap) :
+        ShenandoahMemoryPool(heap,
+                             "Shenandoah") { }
+
+
+MemoryUsage ShenandoahGlobalMemoryPool::get_memory_usage() {
   size_t initial   = initial_size();
   size_t max       = max_size();
   size_t used      = used_in_bytes();
@@ -50,4 +59,78 @@ MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
   assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
 
   return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahGlobalMemoryPool::used_in_bytes() {
+  return _heap->used();
+}
+
+size_t ShenandoahGlobalMemoryPool::max_size() const {
+  return _heap->max_capacity();
+}
+
+
+ShenandoahYoungGenMemoryPool::ShenandoahYoungGenMemoryPool(ShenandoahHeap* heap) :
+        ShenandoahMemoryPool(heap,
+                             "Shenandoah Young Gen") { }
+
+MemoryUsage ShenandoahYoungGenMemoryPool::get_memory_usage() {
+  size_t initial   = initial_size();
+  size_t max       = max_size();
+  size_t used      = used_in_bytes();
+  size_t committed = _heap->young_generation()->used_regions_size();
+
+  // These asserts can never fail: max is stable, and all updates to other values never overflow max.
+  assert(initial <= max,    "initial: "   SIZE_FORMAT ", max: "       SIZE_FORMAT, initial,   max);
+  assert(used <= max,       "used: "      SIZE_FORMAT ", max: "       SIZE_FORMAT, used,      max);
+  assert(committed <= max,  "committed: " SIZE_FORMAT ", max: "       SIZE_FORMAT, committed, max);
+
+  // Committed and used are updated concurrently and independently. They can momentarily break
+  // the assert below, which would also fail in downstream code. To avoid that, adjust values
+  // to make sense under the race. See JDK-8207200.
+  committed = MAX2(used, committed);
+  assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
+
+  return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahYoungGenMemoryPool::used_in_bytes() {
+  return _heap->young_generation()->used();
+}
+
+size_t ShenandoahYoungGenMemoryPool::max_size() const {
+  return _heap->young_generation()->max_capacity();
+}
+
+
+ShenandoahOldGenMemoryPool::ShenandoahOldGenMemoryPool(ShenandoahHeap* heap) :
+        ShenandoahMemoryPool(heap,
+                             "Shenandoah Old Gen") { }
+
+MemoryUsage ShenandoahOldGenMemoryPool::get_memory_usage() {
+  size_t initial   = initial_size();
+  size_t max       = max_size();
+  size_t used      = used_in_bytes();
+  size_t committed = _heap->old_generation()->used_regions_size();
+
+  // These asserts can never fail: max is stable, and all updates to other values never overflow max.
+  assert(initial <= max,    "initial: "   SIZE_FORMAT ", max: "       SIZE_FORMAT, initial,   max);
+  assert(used <= max,       "used: "      SIZE_FORMAT ", max: "       SIZE_FORMAT, used,      max);
+  assert(committed <= max,  "committed: " SIZE_FORMAT ", max: "       SIZE_FORMAT, committed, max);
+
+  // Committed and used are updated concurrently and independently. They can momentarily break
+  // the assert below, which would also fail in downstream code. To avoid that, adjust values
+  // to make sense under the race. See JDK-8207200.
+  committed = MAX2(used, committed);
+  assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
+
+  return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahOldGenMemoryPool::used_in_bytes() {
+  return _heap->old_generation()->used();
+}
+
+size_t ShenandoahOldGenMemoryPool::max_size() const {
+  return _heap->old_generation()->max_capacity();
 }

corretto/corretto-17

src/hotspot/share/gc/shenandoah/shenandoahHeap.cpp
@@ -525,6 +525,8 @@ ShenandoahHeap::ShenandoahHeap(ShenandoahCollectorPolicy* policy) :
   _phase_timings(NULL),
   _monitoring_support(NULL),
   _memory_pool(NULL),
+  _young_gen_memory_pool(NULL),
+  _old_gen_memory_pool(NULL),
   _stw_memory_manager("Shenandoah Pauses", "end of GC pause"),
   _cycle_memory_manager("Shenandoah Cycles", "end of GC cycle"),
   _gc_timer(new (ResourceObj::C_HEAP, mtGC) ConcurrentGCTimer()),
@@ -2602,9 +2604,18 @@ bool ShenandoahHeap::should_inject_alloc_failure() {
 }
 
 void ShenandoahHeap::initialize_serviceability() {
-  _memory_pool = new ShenandoahMemoryPool(this);
-  _cycle_memory_manager.add_pool(_memory_pool);
-  _stw_memory_manager.add_pool(_memory_pool);
+  if (mode()->is_generational()) {
+    _young_gen_memory_pool = new ShenandoahYoungGenMemoryPool(this);
+    _old_gen_memory_pool = new ShenandoahOldGenMemoryPool(this);
+    _cycle_memory_manager.add_pool(_young_gen_memory_pool);
+    _cycle_memory_manager.add_pool(_old_gen_memory_pool);
+    _stw_memory_manager.add_pool(_young_gen_memory_pool);
+    _stw_memory_manager.add_pool(_old_gen_memory_pool);
+  } else {
+    _memory_pool = new ShenandoahMemoryPool(this);
+    _cycle_memory_manager.add_pool(_memory_pool);
+    _stw_memory_manager.add_pool(_memory_pool);
+  }
 }
 
 GrowableArray<GCMemoryManager*> ShenandoahHeap::memory_managers() {
@@ -2616,12 +2627,17 @@ GrowableArray<GCMemoryManager*> ShenandoahHeap::memory_managers() {
 
 GrowableArray<MemoryPool*> ShenandoahHeap::memory_pools() {
   GrowableArray<MemoryPool*> memory_pools(1);
-  memory_pools.append(_memory_pool);
+  if (mode()->is_generational()) {
+    memory_pools.append(_young_gen_memory_pool);
+    memory_pools.append(_old_gen_memory_pool);
+  } else {
+    memory_pools.append(_memory_pool);
+  }
   return memory_pools;
 }
 
 MemoryUsage ShenandoahHeap::memory_usage() {
-  return _memory_pool->get_memory_usage();
+  return MemoryUsage(_initial_size, used(), committed(), max_capacity());
 }
 
 ShenandoahRegionIterator::ShenandoahRegionIterator() :

src/hotspot/share/gc/shenandoah/shenandoahMemoryPool.cpp
@@ -24,13 +24,17 @@
 
 #include "precompiled.hpp"
 #include "gc/shenandoah/shenandoahMemoryPool.hpp"
+#include "gc/shenandoah/shenandoahYoungGeneration.hpp"
+#include "gc/shenandoah/shenandoahOldGeneration.hpp"
 
-ShenandoahMemoryPool::ShenandoahMemoryPool(ShenandoahHeap* heap) :
-        CollectedMemoryPool("Shenandoah",
+ShenandoahMemoryPool::ShenandoahMemoryPool(ShenandoahHeap* heap,
+                                           const char* name) :
+        CollectedMemoryPool(name,
                             heap->initial_capacity(),
                             heap->max_capacity(),
                             true /* support_usage_threshold */),
                             _heap(heap) {}
+
 
 MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
   size_t initial   = initial_size();
@@ -50,4 +54,76 @@ MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
   assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
 
   return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahMemoryPool::used_in_bytes() {
+  return _heap->used();
+}
+
+size_t ShenandoahMemoryPool::max_size() const {
+  return _heap->max_capacity();
+}
+
+ShenandoahYoungGenMemoryPool::ShenandoahYoungGenMemoryPool(ShenandoahHeap* heap) :
+        ShenandoahMemoryPool(heap,
+                             "Shenandoah Young Gen") { }
+
+MemoryUsage ShenandoahYoungGenMemoryPool::get_memory_usage() {
+  size_t initial   = initial_size();
+  size_t max       = max_size();
+  size_t used      = used_in_bytes();
+  size_t committed = _heap->young_generation()->used_regions_size();
+
+  // These asserts can never fail: max is stable, and all updates to other values never overflow max.
+  assert(initial <= max,    "initial: "   SIZE_FORMAT ", max: "       SIZE_FORMAT, initial,   max);
+  assert(used <= max,       "used: "      SIZE_FORMAT ", max: "       SIZE_FORMAT, used,      max);
+  assert(committed <= max,  "committed: " SIZE_FORMAT ", max: "       SIZE_FORMAT, committed, max);
+
+  // Committed and used are updated concurrently and independently. They can momentarily break
+  // the assert below, which would also fail in downstream code. To avoid that, adjust values
+  // to make sense under the race. See JDK-8207200.
+  committed = MAX2(used, committed);
+  assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
+
+  return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahYoungGenMemoryPool::used_in_bytes() {
+  return _heap->young_generation()->used();
+}
+
+size_t ShenandoahYoungGenMemoryPool::max_size() const {
+  return _heap->young_generation()->max_capacity();
+}
+
+ShenandoahOldGenMemoryPool::ShenandoahOldGenMemoryPool(ShenandoahHeap* heap) :
+        ShenandoahMemoryPool(heap,
+                             "Shenandoah Old Gen") { }
+
+MemoryUsage ShenandoahOldGenMemoryPool::get_memory_usage() {
+  size_t initial   = initial_size();
+  size_t max       = max_size();
+  size_t used      = used_in_bytes();
+  size_t committed = _heap->old_generation()->used_regions_size();
+
+  // These asserts can never fail: max is stable, and all updates to other values never overflow max.
+  assert(initial <= max,    "initial: "   SIZE_FORMAT ", max: "       SIZE_FORMAT, initial,   max);
+  assert(used <= max,       "used: "      SIZE_FORMAT ", max: "       SIZE_FORMAT, used,      max);
+  assert(committed <= max,  "committed: " SIZE_FORMAT ", max: "       SIZE_FORMAT, committed, max);
+
+  // Committed and used are updated concurrently and independently. They can momentarily break
+  // the assert below, which would also fail in downstream code. To avoid that, adjust values
+  // to make sense under the race. See JDK-8207200.
+  committed = MAX2(used, committed);
+  assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
+
+  return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahOldGenMemoryPool::used_in_bytes() {
+  return _heap->old_generation()->used();
+}
+
+size_t ShenandoahOldGenMemoryPool::max_size() const {
+  return _heap->old_generation()->max_capacity();
 }
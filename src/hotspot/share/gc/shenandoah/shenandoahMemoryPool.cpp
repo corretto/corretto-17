5bef7e131388fdcdae478db4a4ae19be936e0119
David Alvarez <alvdavi@amazon.com>
2021-08-24 18:41:58 +0000

    Split Generational Shenandoah memory pools

--- src/hotspot/share/gc/shenandoah/shenandoahMemoryPool.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahMemoryPool.cpp
@@ -24,13 +24,17 @@
 
 #include "precompiled.hpp"
 #include "gc/shenandoah/shenandoahMemoryPool.hpp"
+#include "gc/shenandoah/shenandoahYoungGeneration.hpp"
+#include "gc/shenandoah/shenandoahOldGeneration.hpp"
 
-ShenandoahMemoryPool::ShenandoahMemoryPool(ShenandoahHeap* heap) :
-        CollectedMemoryPool("Shenandoah",
+ShenandoahMemoryPool::ShenandoahMemoryPool(ShenandoahHeap* heap,
+                                           const char* name) :
+        CollectedMemoryPool(name,
                             heap->initial_capacity(),
                             heap->max_capacity(),
                             true /* support_usage_threshold */),
                             _heap(heap) {}
+
 
 MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
   size_t initial   = initial_size();
@@ -50,4 +54,76 @@ MemoryUsage ShenandoahMemoryPool::get_memory_usage() {
   assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
 
   return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahMemoryPool::used_in_bytes() {
+  return _heap->used();
+}
+
+size_t ShenandoahMemoryPool::max_size() const {
+  return _heap->max_capacity();
+}
+
+ShenandoahYoungGenMemoryPool::ShenandoahYoungGenMemoryPool(ShenandoahHeap* heap) :
+        ShenandoahMemoryPool(heap,
+                             "Shenandoah Young Gen") { }
+
+MemoryUsage ShenandoahYoungGenMemoryPool::get_memory_usage() {
+  size_t initial   = initial_size();
+  size_t max       = max_size();
+  size_t used      = used_in_bytes();
+  size_t committed = _heap->young_generation()->used_regions_size();
+
+  // These asserts can never fail: max is stable, and all updates to other values never overflow max.
+  assert(initial <= max,    "initial: "   SIZE_FORMAT ", max: "       SIZE_FORMAT, initial,   max);
+  assert(used <= max,       "used: "      SIZE_FORMAT ", max: "       SIZE_FORMAT, used,      max);
+  assert(committed <= max,  "committed: " SIZE_FORMAT ", max: "       SIZE_FORMAT, committed, max);
+
+  // Committed and used are updated concurrently and independently. They can momentarily break
+  // the assert below, which would also fail in downstream code. To avoid that, adjust values
+  // to make sense under the race. See JDK-8207200.
+  committed = MAX2(used, committed);
+  assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
+
+  return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahYoungGenMemoryPool::used_in_bytes() {
+  return _heap->young_generation()->used();
+}
+
+size_t ShenandoahYoungGenMemoryPool::max_size() const {
+  return _heap->young_generation()->max_capacity();
+}
+
+ShenandoahOldGenMemoryPool::ShenandoahOldGenMemoryPool(ShenandoahHeap* heap) :
+        ShenandoahMemoryPool(heap,
+                             "Shenandoah Old Gen") { }
+
+MemoryUsage ShenandoahOldGenMemoryPool::get_memory_usage() {
+  size_t initial   = initial_size();
+  size_t max       = max_size();
+  size_t used      = used_in_bytes();
+  size_t committed = _heap->old_generation()->used_regions_size();
+
+  // These asserts can never fail: max is stable, and all updates to other values never overflow max.
+  assert(initial <= max,    "initial: "   SIZE_FORMAT ", max: "       SIZE_FORMAT, initial,   max);
+  assert(used <= max,       "used: "      SIZE_FORMAT ", max: "       SIZE_FORMAT, used,      max);
+  assert(committed <= max,  "committed: " SIZE_FORMAT ", max: "       SIZE_FORMAT, committed, max);
+
+  // Committed and used are updated concurrently and independently. They can momentarily break
+  // the assert below, which would also fail in downstream code. To avoid that, adjust values
+  // to make sense under the race. See JDK-8207200.
+  committed = MAX2(used, committed);
+  assert(used <= committed, "used: "      SIZE_FORMAT ", committed: " SIZE_FORMAT, used,      committed);
+
+  return MemoryUsage(initial, used, committed, max);
+}
+
+size_t ShenandoahOldGenMemoryPool::used_in_bytes() {
+  return _heap->old_generation()->used();
+}
+
+size_t ShenandoahOldGenMemoryPool::max_size() const {
+  return _heap->old_generation()->max_capacity();
 }
