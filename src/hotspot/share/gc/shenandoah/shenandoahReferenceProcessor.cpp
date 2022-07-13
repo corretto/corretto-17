3e42b69d9104796df26e1c0801f6eb6e2844ff0a
Kelvin Nilsen <kdnilsen@openjdk.org>
2022-07-13 16:59:38 +0000

    Refactor budgeting to make the logic cleaner
    Reviewed-by: rkennke, wkemper

--- src/hotspot/share/gc/shenandoah/shenandoahReferenceProcessor.cpp
+++ src/hotspot/share/gc/shenandoah/shenandoahReferenceProcessor.cpp
@@ -548,7 +548,6 @@ void ShenandoahReferenceProcessor::enqueue_references(bool concurrent) {
     // Nothing to enqueue
     return;
   }
-
   if (!concurrent) {
     // When called from mark-compact or degen-GC, the locking is done by the VMOperation,
     enqueue_references_locked();
