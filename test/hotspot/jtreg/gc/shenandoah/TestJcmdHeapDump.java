09467bdb313ada80e210ca942397a59af90fd8b0
William Kemper <kemperw@amazon.com>
2023-05-11 17:03:25 -0700

    Restrict generational mode to adaptive heuristic
    Reviewed-by: shade

--- test/hotspot/jtreg/gc/shenandoah/TestJcmdHeapDump.java
+++ test/hotspot/jtreg/gc/shenandoah/TestJcmdHeapDump.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2017, 2018, Red Hat, Inc. All rights reserved.
+ * Copyright (c) 2021, Red Hat, Inc. All rights reserved.
  * DO NOT ALTER OR REMOVE COPYRIGHT NOTICES OR THIS FILE HEADER.
  *
  * This code is free software; you can redistribute it and/or modify it
@@ -24,130 +24,150 @@
 
 /*
  * @test id=passive
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  * @requires vm.gc.Shenandoah
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCMode=passive
  *      -XX:+ShenandoahDegeneratedGC
- *      -Dtarget=10000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCMode=passive
  *      -XX:-ShenandoahDegeneratedGC
- *      -Dtarget=10000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  */
 
 /*
  * @test id=aggressive
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  * @requires vm.gc.Shenandoah
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=aggressive
  *      -XX:+ShenandoahOOMDuringEvacALot
- *      -Dtarget=1000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=aggressive
  *      -XX:+ShenandoahAllocFailureALot
- *      -Dtarget=1000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=aggressive
- *      -Dtarget=1000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  */
 
 /*
  * @test id=adaptive
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  * @requires vm.gc.Shenandoah
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=adaptive
  *      -Dtarget=10000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  */
 
 /*
  * @test id=generational
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  * @requires vm.gc.Shenandoah
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=adaptive -XX:ShenandoahGCMode=generational
  *      -Dtarget=10000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  */
 
 /*
  * @test id=static
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  * @requires vm.gc.Shenandoah
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=static
- *      -Dtarget=10000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  */
 
 /*
  * @test id=compact
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  * @requires vm.gc.Shenandoah
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=compact
- *      -Dtarget=1000
- *     TestLotsOfCycles
+ *     TestJcmdHeapDump
  */
 
 /*
  * @test id=iu-aggressive
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  * @requires vm.gc.Shenandoah
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCMode=iu -XX:ShenandoahGCHeuristics=aggressive
  *      -XX:+ShenandoahOOMDuringEvacALot
- *      -Dtarget=1000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCMode=iu -XX:ShenandoahGCHeuristics=aggressive
  *      -XX:+ShenandoahAllocFailureALot
- *      -Dtarget=1000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCMode=iu -XX:ShenandoahGCHeuristics=aggressive
- *      -Dtarget=1000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  */
 
 /*
  * @test id=iu
  * @requires vm.gc.Shenandoah
+ * @library /test/lib
+ * @modules jdk.attach/com.sun.tools.attach
  *
  * @run main/othervm/timeout=480 -Xmx16m -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCMode=iu
- *      -Dtarget=10000
- *      TestLotsOfCycles
+ *      TestJcmdHeapDump
  */
 
-public class TestLotsOfCycles {
+import jdk.test.lib.JDKToolLauncher;
+import jdk.test.lib.process.OutputAnalyzer;
 
-    static final long TARGET_MB = Long.getLong("target", 10_000); // 10 Gb allocation, around 1K cycles to handle
-    static final long STRIDE = 100_000;
+import java.io.File;
 
-    static volatile Object sink;
+public class TestJcmdHeapDump {
+    public static void main(String[] args) {
+        long pid = ProcessHandle.current().pid();
+        JDKToolLauncher jcmd = JDKToolLauncher.createUsingTestJDK("jcmd");
+        jcmd.addToolArg(String.valueOf(pid));
+        jcmd.addToolArg("GC.heap_dump");
+        String dumpFileName = "myheapdump" + String.valueOf(pid);
+        jcmd.addToolArg(dumpFileName);
 
-    public static void main(String[] args) throws Exception {
-        long count = TARGET_MB * 1024 * 1024 / 16;
-        for (long c = 0; c < count; c += STRIDE) {
-            for (long s = 0; s < STRIDE; s++) {
-                sink = new Object();
-            }
-            Thread.sleep(1);
+        try {
+            ProcessBuilder pb = new ProcessBuilder(jcmd.getCommand());
+            Process jcmdProc = pb.start();
+
+            OutputAnalyzer output = new OutputAnalyzer(jcmdProc);
+            jcmdProc.waitFor();
+            output.shouldHaveExitValue(0);
+        } catch (Exception e) {
+            throw new RuntimeException("Test failed: " + e);
+        }
+
+        File f = new File(dumpFileName);
+        if (f.exists()) {
+            f.delete();
+        } else {
+            throw new RuntimeException("Dump file not created");
         }
     }
-
 }
