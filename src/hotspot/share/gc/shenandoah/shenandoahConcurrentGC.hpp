e7002e2aba23632da11d0c7ae3b245ac8d81a3e0
William Kemper <kemperw@amazon.com>
2023-05-11 17:04:46 -0700

    Restore event descriptions immortality
    Reviewed-by: kdnilsen, ysr

--- src/hotspot/share/gc/shenandoah/shenandoahConcurrentGC.hpp
+++ src/hotspot/share/gc/shenandoah/shenandoahConcurrentGC.hpp
@@ -129,11 +129,12 @@ private:
 protected:
   virtual void op_final_mark();
 
+private:
   // Messages for GC trace events, they have to be immortal for
   // passing around the logging/tracing systems
-  void init_mark_event_message(char* buf, size_t len) const;
-  void final_mark_event_message(char* buf, size_t len) const;
-  void conc_mark_event_message(char* buf, size_t len) const;
+  const char* init_mark_event_message() const;
+  const char* final_mark_event_message() const;
+  const char* conc_mark_event_message() const;
 
 protected:
   // Check GC cancellation and abort concurrent GC
