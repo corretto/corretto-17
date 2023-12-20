7bdab83fd43d3148b645335995b99ab8ac87b861
William Kemper <kemperw@amazon.com>
2023-12-20 15:22:05 -0800

    Revert "8322242: [GenShen] TestAllocObjects#generational fails with "Unrecognized VM option 'ShenandoahSuspendibleWorkers'""
    This reverts commit da2b9ed1520b6c152e019f7c333095b27c48d8cf.

--- test/hotspot/jtreg/gc/shenandoah/TestAllocObjects.java
+++ test/hotspot/jtreg/gc/shenandoah/TestAllocObjects.java
@@ -118,6 +118,11 @@
  * @run main/othervm -Xmx1g -Xms1g -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
  *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=adaptive -XX:ShenandoahGCMode=generational
  *      TestAllocObjects
+ *
+ * @run main/othervm -Xmx1g -Xms1g -XX:+UnlockDiagnosticVMOptions -XX:+UnlockExperimentalVMOptions
+ *      -XX:+UseShenandoahGC -XX:ShenandoahGCHeuristics=adaptive -XX:ShenandoahGCMode=generational
+ *      -XX:+ShenandoahSuspendibleWorkers
+ *      TestAllocObjects
  */
 
 /*
