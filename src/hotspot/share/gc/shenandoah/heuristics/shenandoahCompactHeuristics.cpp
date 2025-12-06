2103f9368ca785733ec50aebd91e60ea5701f9b1
Rui Li <ruiamzn@amazon.com>
2025-12-06 00:20:51 +0000

    8372543: Shenandoah: undercalculated the available size when soft max takes effect

--- src/hotspot/share/gc/shenandoah/heuristics/shenandoahCompactHeuristics.cpp
+++ src/hotspot/share/gc/shenandoah/heuristics/shenandoahCompactHeuristics.cpp
@@ -46,25 +46,27 @@ ShenandoahCompactHeuristics::ShenandoahCompactHeuristics() : ShenandoahHeuristic
 
 bool ShenandoahCompactHeuristics::should_start_gc() {
   ShenandoahHeap* heap = ShenandoahHeap::heap();
-
+  size_t allocated = heap->bytes_allocated_since_gc_start();
   size_t max_capacity = heap->max_capacity();
-  size_t capacity = heap->soft_max_capacity();
-  size_t available = heap->free_set()->available();
+  size_t soft_max_capacity = heap->soft_max_capacity();
+  size_t soft_mutator_capacity = soft_max_capacity * (100.0 - ShenandoahEvacReserve) / 100;
 
-  // Make sure the code below treats available without the soft tail.
-  size_t soft_tail = max_capacity - capacity;
-  available = (available > soft_tail) ? (available - soft_tail) : 0;
-
-  size_t threshold_bytes_allocated = capacity / 100 * ShenandoahAllocationThreshold;
-  size_t min_threshold = capacity / 100 * ShenandoahMinFreeThreshold;
+  size_t used = heap->free_set()->used();
+  size_t available = (soft_mutator_capacity > used) ? soft_mutator_capacity - used : 0;
+  size_t min_threshold = soft_max_capacity / 100 * ShenandoahMinFreeThreshold;
 
   if (available < min_threshold) {
-    log_info(gc)("Trigger: Free (" SIZE_FORMAT "%s) is below minimum threshold (" SIZE_FORMAT "%s)",
-                 byte_size_in_proper_unit(available),     proper_unit_for_byte_size(available),
-                 byte_size_in_proper_unit(min_threshold), proper_unit_for_byte_size(min_threshold));
+    log_debug(gc, ergo)("should_start_gc calculation: available: " PROPERFMT ", soft_max_capacity: "  PROPERFMT ", "
+                  "allocated_since_gc_start: "  PROPERFMT,
+                  PROPERFMTARGS(available), PROPERFMTARGS(soft_max_capacity), PROPERFMTARGS(allocated));
+
+
+    log_info(gc)("Trigger: Free (Soft) (" PROPERFMT ") is below minimum threshold (" PROPERFMT ")",
+                 PROPERFMTARGS(available), PROPERFMTARGS(min_threshold));
     return true;
   }
 
+  size_t threshold_bytes_allocated = soft_max_capacity / 100 * ShenandoahAllocationThreshold;
   size_t bytes_allocated = heap->bytes_allocated_since_gc_start();
   if (bytes_allocated > threshold_bytes_allocated) {
     log_info(gc)("Trigger: Allocated since last cycle (" SIZE_FORMAT "%s) is larger than allocation threshold (" SIZE_FORMAT "%s)",
